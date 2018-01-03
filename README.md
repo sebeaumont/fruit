efc4d7bc4b0237bc9e2ac435454aeaa936ff740d
Simon Beaumont <s@molemind.net>
2018-01-03 14:31:10 +0000

    Basic tokenizer and sample frame buffer

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 Deep Learning of Narrative
-==========================
+===========================
 
-1. Input docs as json lines
-   lazy or one line at a time?
+- Input docs as json lines
+  lazy or one line at a time? latter done.
    
-2. tokenize
-   capitals (char-rnn annotation trick?), punctuation, numbers, 
-   
-3. rare vs. common words/terms
+- tokenize
+  capitalization (char-rnn annotation trick?), punctuation, numbers, 
+  light touch done.
 
-4. word2vec vs. semantic embeddings
-   why is word to vec handwavy about semantic localtity
-   when DSR is clear -- shoule we use DSR?
-   
-4. char-rnn vs. word-rnn (LTST)
-   word on the street is word for performance and profit
+- Frames TODO
+	- output direclty to frametrain if using sdr pro-tem re-engineer later. 
+	- What does word2vec do?
+  
+- EMBEDDINGS word2vec vs. SDR
+	- why is word2vec hand-wavy about semantic locality
+	- SDR is clear should we use SDR? downsampling SDRs/or sparse rep as they are very big input space
+	- what does word2vec do?
+
+- TERMS rare vs. common words filtering/folding?
+
+- LSTM word sequence model
+	- char-RNN vs. word-RNN word on the street is word for performance and profit.
+
+- HOLD ON -- what do we need this LANGUAGE model for?
+  what can we do with it? What can we ask it? 
+  How does it help?
+
+
    
--- tokenize.hs
+++ tokenize.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-import Control.Monad (when)
+
 import System.IO (isEOF)
 import Data.Aeson
-import Data.Text
+import Data.Char
+import qualified Data.Text as T
 import qualified Data.ByteString as B
+--import qualified Data.Sequence as S
+--import Debug.Trace
+
 
 -- | Record to represent a general document we might want to add an
 -- optional corpus to this record...
-
 data Document = Document {
   docid :: String,
-  content  :: Text
+  content  :: T.Text
   } deriving (Show)
 
 instance FromJSON Document where
@@ -22,13 +25,11 @@ instance FromJSON Document where
     <*> v .: "content"
 
 -- main entry point
-
 main :: IO ()
 main = iolines 0 >>= print
 
 -- | Read and count lines from stdin dispatching data for Aeson to parse
 -- bad JSON decodes are discarded and errors logged with line number.
-
 iolines :: Int -> IO Int
 iolines n = do
   ateof <- isEOF
@@ -37,15 +38,52 @@ iolines n = do
     else B.getLine >>= decodeDocument n
          >> iolines (n+1)
 
--- | Attempt decode of data to Document record logging any errors
 
+-- | Attempt to decode nth. line of data to a Document record logging any
+-- errors or processing document.
 decodeDocument :: Int -> B.ByteString -> IO ()
 decodeDocument n s =
   case eitherDecodeStrict' s :: Either String Document of
-    Left e -> putStrLn $ e ++ " at line: " ++ show n
+    Left e -> putStrLn $ e ++ " at line: " ++ show (n+1)
     Right d -> processDocument d
 
+-- | test output driver
 processDocument :: Document -> IO ()
-processDocument = print
+processDocument d = 
+  let buffer = [] in
+    print $ samples [cleanToken t | t <- tokenizeDocument d] 7
 
-  
+
+-- | Simple white space splitter turns a document into a list of tokens
+tokenizeDocument :: Document -> [T.Text]
+tokenizeDocument = T.words . content
+
+-- | Text words need some cleanup
+cleanToken :: T.Text -> T.Text
+cleanToken = T.dropAround isPunctuation  
+
+
+--
+-- experimental cotraining
+--
+
+type Frame = [T.Text] 
+
+samples :: [T.Text] -> Int -> Double  
+samples [] _ = 0.0
+samples (t:ts) n  = sample t (take n ts) + samples ts n
+
+
+{-
+-- instrumented with naughty function...
+isample :: T.Text -> Frame -> Double
+isample t f = trace (show t ++ "\t" ++ show f) (sample t f)
+-}
+
+-- | TODO with this sample buffer i.e. superpose all of frame
+-- members with t and superpose t with all frame members.
+-- should have good cache behaviour and arithmetic density
+
+sample :: T.Text -> Frame -> Double
+sample t f = 1.0 -- we could return some property of the sample vector here
+    
