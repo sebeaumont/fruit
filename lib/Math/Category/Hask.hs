5ea89500e907d28708347b7727176fc01f623b07
Simon Beaumont <simon.beaumont@clinithink.com>
2018-01-26 12:27:58 +0000

    refactorings removed Hask pedagogical code

--- lib/Math/Category/Hask.hs
+++
@@ -1,7 +0,0 @@
-
-module Math.Category.Hask ( module Math.Category.Hask.Functor
-                          , module Math.Category.Hask.State
-                          ) where
-
-import Math.Category.Hask.Functor
-import Math.Category.Hask.State
--- tokenize.hs
+++ tokenize.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
---{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 -- | Main program to process input documents, vectorize and send to ML backends
 -- test bed for encoder and ML modules.
@@ -8,10 +8,10 @@
 
 module Main where
 
-import Control.Monad (when, unless, void)
+--import Control.Monad (when, unless, void)
 import Control.Monad.State
 
-import System.IO (isEOF)
+import System.IO (isEOF, hPutStrLn, stderr)
 import System.Console.CmdArgs
 
 import Data.Aeson
@@ -19,7 +19,7 @@ import Data.Char
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+--import qualified Data.Text.IO as TIO
 import qualified Data.ByteString as B
 
 
@@ -30,17 +30,103 @@ data Document = Document {
   content  :: !T.Text
   } deriving (Show)
 
+{-
 instance FromJSON Document where
   parseJSON (Object v) =
     Document
     <$> v .: "docid"
     <*> v .: "content"
+  -- this function definitely not total for all json not sure how to handle that!
+-}
+
+instance FromJSON Document where
+  parseJSON = withObject "Document" $ \v -> Document
+    <$> v .: "docid"
+    <*> v .: "content"
+
+-- | cheap logger
+logerr :: String -> IO ()
+logerr = hPutStrLn stderr
+
+-- command line parameters
+data Tokenizer = Tokenizer {
+  indexTokens :: [T.Text],
+    forward :: Integer,
+    behind :: Integer
+  } deriving (Show, Data, Typeable)
+
+
+defaultToz :: Tokenizer
+defaultToz = Tokenizer [] 7 7
 
 -- main entry point
 main :: IO ()
-main = void $ iolines 0 
+-- TODO get parameters from command line
+main = do
+  opts <- cmdArgs defaultToz
+  execStateT proio (tokenMap $ indexTokens opts) >>= print
+
+-- | This map of tokens allow us to keep track of specific token instances
+type TokenMap = Map.Map T.Text Int
 
 
+-- | Make new token map from list of tokens
+tokenMap :: [T.Text]  -> TokenMap
+tokenMap l = Map.fromList [(t, 0) | t <- l]
+
+----------------------------------------------------
+-- monadic io side effects with state of TokenMap --
+----------------------------------------------------
+
+-- | replacement io loop
+-- call this to process a corpus of json lines via stdio
+proio :: StateT TokenMap IO ()
+proio = do
+  eof <- lift isEOF
+  if eof
+    then return ()
+    else do
+      lift B.getLine >>= (processDocumentM . decodeDocument)
+      proio
+
+-- | process a set of tokens as a document also report any
+-- decode errors to stdio (need line numbers back) 
+processDocumentM :: Either String [T.Text] -> StateT TokenMap IO ()
+processDocumentM r =
+  case r of
+    Left s -> lift $ logerr s -- this is a joke but ok in this case
+    Right ts -> do
+      framesM 7 $ tokenize ts
+      return ()
+
+-- | Frames of n from Int 
+framesM :: Int -> [T.Text] -> StateT TokenMap IO ()
+framesM _ [] = return ()
+framesM n (t:ts) = frameM t (take n ts) >> framesM n ts
+
+-- | Training pairs from frame (1 target with rest source)
+frameM :: T.Text -> [T.Text] -> StateT TokenMap IO ()
+frameM t = mapM_ (pairM t)
+
+-- | Dump sample pairs to stdout
+-- N.B. we could encodeUtf8 these to byte strings...
+pairM :: T.Text -> T.Text -> StateT TokenMap IO (T.Text, T.Text)
+pairM t s = do
+  t' <- tagTokenM t
+  s' <- tagTokenM s
+  lift (putStrLn $ (T.unpack t') ++ "\t" ++ (T.unpack s') ++ "\t" ++ show (1.0 :: Double))
+  return (t', s')
+
+-- | Monadic wrapper for stateful token transactions
+tagTokenM :: T.Text -> StateT TokenMap IO T.Text 
+tagTokenM t =
+  do tm <- get
+     let (m, v) = tagToken tm t 
+     put m
+     return v
+
+
+-- the original io loop
 
 -- | Read and count lines from stdin dispatching data for Aeson to parse
 -- bad JSON decodes are discarded and errors logged with line number.
@@ -51,8 +137,6 @@ iolines n = do
     then return n
     else B.getLine >>= (processDocument . decodeDocument)
          >> iolines (n+1)
-
-
 
 -- | Attempt to decode nth. line of data to a Document record returning documents
 -- tokens or description of decode error
@@ -75,11 +159,11 @@ supressChars :: Char -> Bool
 supressChars c = or [isPunctuation c, isNumber c, isSymbol c]  
 
 rejectToken :: T.Text -> Bool
-rejectToken t = T.null t || len t <2 || len t >20 || allSame t
+rejectToken t = T.null t || len t <2 || len t >20 || allSameChar t
   where
     len = T.length
-    allSame :: T.Text -> Bool
-    allSame t = T.all (== T.head t) t 
+    allSameChar :: T.Text -> Bool
+    allSameChar s = T.all (== T.head s) s 
   
 acceptToken :: T.Text -> Bool
 acceptToken = not . rejectToken 
@@ -89,28 +173,15 @@ tokenize :: [T.Text] -> [T.Text]
 tokenize ts = filter acceptToken $ map cleanToken ts
 
 
--- | This map of tokens allow us to keep track of specific tokens
-type TokenMap = Map.Map T.Text Int
-
-
--- | Make new token map from list of tokens
-tokenMap :: [T.Text]  -> TokenMap
-tokenMap l = Map.fromList [(t, 0) | t <- l]
-
-
--- | Append a unique tag to a token if it is in our map WIP/2DO
--- TokenMap state so we can modify iff changed update the counter for
--- the map entry and return new map and token
+-- | Append a unique tag to a token if it is in the given map
+-- bump the counter in the map entry and return new map and token thus effected
 tagToken :: TokenMap -> T.Text -> (TokenMap, T.Text)
 tagToken tm t =
-  let (v, m) = Map.updateLookupWithKey (\k x -> Just (x + 1)) t tm in
+  let (v, m) = Map.updateLookupWithKey (\_ x -> Just (x + 1)) t tm in
     case v of
       Nothing -> (tm, t) 
       Just n -> (m, T.append t (T.pack $ show n))
 
---
--- its all IO to output samples here on...
---
 
 -- | Report error or process frames
 -- TODO pass in tokenizer state so we can update it
@@ -119,9 +190,8 @@ tagToken tm t =
 processDocument :: Either String [T.Text] -> IO ()
 processDocument r = 
   case r of
-    Left s -> putStrLn s
+    Left s -> logerr s
     Right ts -> ((frames 7) . tokenize) ts
-
 
 
 -- | Make frames from document tokens
@@ -138,57 +208,10 @@ frame t = mapM_ (pair t)
 -- | Dump to stdout
 -- N.B. we could encodeUtf8 these to byte strings...
 pair :: T.Text -> T.Text -> IO ()
-pair t s = putStrLn $ (T.unpack t) ++ "\t" ++ (T.unpack s) ++ "\t" ++ show 1.0
+pair t s = putStrLn $ (T.unpack t) ++ "\t" ++ (T.unpack s) ++ "\t" ++ show (1.0 :: Double)
 
 
 
 sampleText :: T.Text
 sampleText = "PubMed:13211925        Various strains of influenza virus produce a cytopathogenic effect in cultures of HeLa cells. The virus could not be passed in series. Virus partially or even completely inactivated with respect to infectivity by exposure to 37 degrees C. or ultraviolet light retained some of its cytopathogenic effect. No evidence has been obtained of an increase in infectious virus in HeLa cultures, but an increase in hemagglutinins and in both viral and soluble complement-fixing antigens became detectable during incubation. These virus materials apparently were not released from these cells prior to their destruction. These results suggested that HeLa cells are capable of supporting an incomplete reproductive cycle of influenza virus. The fact that radioactive phosphorus was readily incorporated into the hemagglutinin supplies strong evidence for this interpretation."
 
-----------------
--- hack alert --
-----------------
-
--- using state monad to wrap the tokenmap and use io in the same monad
-
-sampleMap :: TokenMap 
-sampleMap = tokenMap ["virus", "or","that","for","but"]
-
-proio :: StateT TokenMap IO ()
-proio = do
-  s <- lift B.getLine
-  let d = decodeDocument s
-  x <- processDocumentM d
-  return ()
-
-processDocumentM :: Either String [T.Text] -> StateT TokenMap IO ()
-processDocumentM r =
-  case r of
-    Left s -> return () -- XX really need to return this error liftIO $ putStrLn s
-    Right ts -> do
-      framesM 7 $ tokenize ts
-      return ()
-
-framesM :: Int -> [T.Text] -> StateT TokenMap IO ()
-framesM _ [] = return ()
-framesM n (t:ts) = frameM t (take n ts) >> framesM n ts
-
--- | Training pairs from frame (1 target with rest source)
-frameM :: T.Text -> [T.Text] -> StateT TokenMap IO ()
-frameM t = mapM_ (pairM t)
-
--- | Dump to stdout
--- N.B. we could encodeUtf8 these to byte strings...
-pairM :: T.Text -> T.Text -> StateT TokenMap IO (T.Text, T.Text)
-pairM t s = do
-  t' <- tagTokenM t
-  s' <- tagTokenM s
-  return (t', s')
-
--- | Monadic wrapper for stateful token transactions
-tagTokenM :: T.Text -> StateT TokenMap IO T.Text 
-tagTokenM t =
-  do tm <- get
-     let (m, v) = tagToken tm t 
-     put m
-     return v
